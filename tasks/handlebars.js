8ce226b6fd9d5ad2243f881952f76bc1b6462921
Luc Castera <luc.castera@gmail.com>
2012-06-14 16:08:19 -0400

    Made task async

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -12,9 +12,10 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('handlebars', 'Precompile Handlebars template', function() {
     var self = this;
+    var done = self.async();
     var templateDir = this.file.src;
 
     var handlebarsCmd = __dirname + '/../node_modules/.bin/handlebars -m ' + templateDir + '/*.handlebars -f ' + this.file.dest;
-    exec(handlebarsCmd);
+    exec(handlebarsCmd, done);
   });
 };
