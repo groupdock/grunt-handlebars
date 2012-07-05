59f9f00ab40609e756e7ad229d5503ff99df6a93
Tim Oxley <secoif@gmail.com>
2012-07-06 04:34:49 +1000

    truncate destination file before running grunt command to error harder.

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -14,9 +14,9 @@ module.exports = function(grunt) {
     var self = this;
     var done = self.async();
     var templateDir = this.file.src;
-
+    var truncateFileCmd = '> ' +this.file.dest;
     var handlebarsCmd = __dirname + '/../node_modules/.bin/handlebars -m ' + templateDir + '/*.handlebars -f ' + this.file.dest;
-    exec(handlebarsCmd, function(err, stdout, stderr) {
+    exec(truncateFileCmd +' && '+ handlebarsCmd, function(err, stdout, stderr) {
       if (err) {
         grunt.fail.fatal(stderr);
       }
